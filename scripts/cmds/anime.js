7fa3519c0dccb8bb531dcdad26b9c4e7ba2e3e20
Msk <sahariyam13@gmail.com>
2026-08-31 02:35:18 +0600

    Update anime.js

--- scripts/cmds/anime.js
+++ scripts/cmds/anime.js
@@ -37,21 +37,21 @@ module.exports = {
                         wait: "🐤 | এনিমে ভিডিও লোড হচ্ছে... একটু অপেক্ষা করো বেবি! <😘",
                         noVid: "× কোনো ভিডিও খুঁজে পাওয়া যায়নি!",
                         success: "✨ | 𝐇𝐞𝐫𝐞'𝐬 𝐲𝐨𝐮𝐫 𝐚𝐧𝐢𝐦𝐞 𝐯𝐢𝐝𝐞𝐨 𝐛𝐚𝐛𝐲 <😘",
-                        error: "× সমস্যা হয়েছে: %1। প্রয়োজনে Contact MahMUD।\n•WhatsApp: 01836298139"
+                        error: "× সমস্যা হয়েছে: %1। "
                 },
                 en: {
                         noCat: "× No anime categories found.",
                         wait: "🐤 | Loading random anime video... Please wait baby! <😘",
                         noVid: "× No videos found.",
                         success: "✨ | 𝐇𝐞𝐫𝐞'𝐬 𝐲𝐨𝐮𝐫 𝐚𝐧𝐢𝐦𝐞 𝐯𝐢𝐝𝐞𝐨 𝐛𝐚𝐛𝐲 <😘",
-                        error: "× API error: %1. Contact MahMUD for help.\n•WhatsApp: 01836298139"
+                        error: "× API error: %1."
                 },
                 vi: {
                         noCat: "× Không tìm thấy danh mục anime nào.",
                         wait: "🐤 | Đang tải video anime... Chờ chút nhé cưng! <😘",
                         noVid: "× Không tìm thấy video nào.",
                         success: "✨ | Video anime của cưng đây <😘",
-                        error: "× Lỗi: %1. Liên hệ MahMUD để hỗ trợ.\n•WhatsApp: 01836298139"
+                        error: "× Lỗi: %1."
                 }
         },
 
