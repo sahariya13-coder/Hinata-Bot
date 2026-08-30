c209788270a8d33db05f7be96dab96eb6de6b70e
Msk <sahariyam13@gmail.com>
2026-08-31 02:30:09 +0600

    Remove specific error messages from animal.js

--- scripts/cmds/animal.js
+++ scripts/cmds/animal.js
@@ -32,21 +32,21 @@ module.exports = {
                         correct: "✅ | একদম সঠিক উত্তর বেবি!\n\nতুমি জিতেছো %1 কয়েন এবং %2 এক্সপি।",
                         wrong: "🥺 | উত্তরটি ভুল হয়েছে বেবি!\n\nসঠিক উত্তর ছিল: %1",
                         notYour: "× বেবি, এটি তোমার জন্য নয়! নিজের জন্য গেম শুরু করো। >🐸",
-                        error: "× সমস্যা হয়েছে: %1। প্রয়োজনে Contact MahMUD।"
+                        error: "× সমস্যা হয়েছে: %1।"
                 },
                 en: {
                         start: "A random animal has appeared! Guess the name, baby.",
                         correct: "✅ | Correct answer, baby!\n\nYou have earned %1 coins and %2 exp.",
                         wrong: "🥺 | Wrong Answer, baby!\n\nThe Correct answer was: %1",
                         notYour: "× This is not your game, baby! >🐸",
-                        error: "× API error: %1. Contact MahMUD for help."
+                        error: "× API error: %1."
                 },
                 vi: {
                         start: "Một con vật đã xuất hiện! Đoán tên đi cưng.",
                         correct: "✅ | Đáp án chính xác cưng ơi!\n\n✨ Bạn nhận được %1 xu và %2 exp.",
                         wrong: "🥺 | Sai rồi cưng ơi!\n\nĐáp án đúng là: %1",
                         notYour: "× Đây không phải phần chơi của bạn cưng à! >🐸",
-                        error: "× Lỗi: %1. Liên hệ MahMUD để được hỗ trợ."
+                        error: "× Lỗi: %1."
                 }
         },
 
