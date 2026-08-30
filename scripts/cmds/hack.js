b26a4073f6a8adcbe827f0453b3a9578f89972c6
Msk <sahariyam13@gmail.com>
2026-08-31 03:26:10 +0600

    Update hack.js

--- scripts/cmds/hack.js
+++ scripts/cmds/hack.js
@@ -32,11 +32,11 @@ module.exports = {
         langs: {
                 bn: {
                         success: "✅ এই ইউজারকে সফলভাবে হ্যাক করা হয়েছে: %1",
-                        error: "× হ্যাক করতে গিয়ে সমস্যা হয়েছে: %1। প্রয়োজনে Contact MahMUD।\n•WhatsApp: 01836298139"
+                        error: "× হ্যাক করতে গিয়ে সমস্যা হয়েছে: %1।"
                 },
                 en: {
                         success: "✅ Successfully Hacked This User: %1",
-                        error: "× Failed to hack: %1. Contact MahMUD for help.\n•WhatsApp: 01836298139"
+                        error: "× Failed to hack: %1."
                 }
         },
 
