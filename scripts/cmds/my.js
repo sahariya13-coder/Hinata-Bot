92d51081aba0cc7e0906d0d9708e5c0b687c695e
Msk <sahariyam13@gmail.com>
2026-08-31 03:34:09 +0600

    Update my.js

--- scripts/cmds/my.js
+++ scripts/cmds/my.js
@@ -93,7 +93,7 @@ module.exports = {
           event.messageID
         );
       } catch (err) {
-        api.sendMessage("🥹error, contact MahMUD. " + err.message, event.threadID);
+        api.sendMessage("🥹error,. " + err.message, event.threadID);
       }
     }
   }
